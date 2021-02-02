621b5ef3261b7751fbb16814e921b6553d6ca69c
winkerVSbecks <varunvachhar@gmail.com>
2021-02-02 09:36:14 -0500

    use {} in template.bind

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -18,13 +18,13 @@ const Template = (args) => <Button {...args}>Label</Button>;
 
 export const All = () => <Button>Normal</Button>;
 
-export const Default = Template.bind();
+export const Default = Template.bind({});
 
-export const Hover = Template.bind();
+export const Hover = Template.bind({});
 Hover.args = { pseudo: { hover: true } };
 
-export const Focus = Template.bind();
+export const Focus = Template.bind({});
 Focus.args = { pseudo: { focus: true } };
 
-export const Active = Template.bind();
+export const Active = Template.bind({});
 Active.args = { pseudo: { active: true } };
