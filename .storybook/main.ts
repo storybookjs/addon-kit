da6c3aaaf2d424df4bf9e405d5085e0a2438c17c
Valentin Palkovic <valentin@chromatic.com>
2025-03-25 10:53:37 +0100

    Remove addon-interactions

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,7 +4,6 @@ const config: StorybookConfig = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
     "./local-preset.js",
   ],
   framework: {
