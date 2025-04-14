2e3f6e3a5bfc0def829f760b906515f67f8c0e5b
Norbert de Langen <ndelangen@me.com>
2025-04-14 16:43:27 +0200

    Update Storybook configuration and dependencies
    - Replaced `@storybook/addon-essentials` and `@storybook/addon-interactions` with `@storybook/addon-docs` in package.json and .storybook/main.ts.
    - Updated import statements in Introduction.mdx to use `@storybook/addon-docs/blocks`.
    - Renamed the postinstall script in package.json to `_postinstall` for compatibility.
    - Updated various dependencies in package-lock.json to their latest versions, including `@auto-it` packages and Babel-related packages.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,16 +3,10 @@ const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    "@storybook/addon-docs",
     "./local-preset.js",
   ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+  framework: { name: "@storybook/react-vite", options: {} },
+  docs: { autodocs: "tag" },
 };
 export default config;
