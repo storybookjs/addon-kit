272be2661e13091b61b87c436fe6c68f6e988da9
Jeppe Reinhold <jeppe@reinhold.is>
2025-05-28 15:07:53 +0200

    upgrade to SB 9.0.0

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,9 @@
-import type { StorybookConfig } from "@storybook/react-vite";
-const config: StorybookConfig = {
+import { defineMain } from "@storybook/react-vite/node";
+
+const config = defineMain({
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-docs",
-    "./local-preset.cjs",
-  ],
+  addons: ["@storybook/addon-docs", "./local-preset.cjs"],
   framework: "@storybook/react-vite",
-};
+});
+
 export default config;
