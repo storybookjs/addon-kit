e1cd97aa761cf5b564bf88b77df1924daf86c9f4
Jeppe Reinhold <jeppe@reinhold.is>
2025-07-21 16:10:43 +0200

    cleanup comments

--- tsup.config.ts
+++ tsup.config.ts
@@ -21,20 +21,28 @@ export default defineConfig(async (options) => {
   } = packageJson;
 
   const commonConfig: Options = {
-    // keep this line commented until https://github.com/egoist/tsup/issues/1270 is resolved
-    // clean: options.watch ? false : true,
+    /*
+     keep this line commented until https://github.com/egoist/tsup/issues/1270 is resolved
+     clean: options.watch ? false : true,
+    */
     clean: false,
     format: ["esm"],
     treeshake: true,
     splitting: true,
+    /*
+     The following packages are provided by Storybook and should always be externalized
+     Meaning they shouldn't be bundled with the addon, and they shouldn't be regular dependencies either
+    */
     external: ["react", "react-dom", "@storybook/icons"],
   };
 
   const configs: Options[] = [];
 
-  // manager entries are entries meant to be loaded into the manager UI
-  // they'll have manager-specific packages externalized and they won't be usable in node
-  // they won't have types generated for them as they're usually loaded automatically by Storybook
+  /*
+   manager entries are entries meant to be loaded into the manager UI
+   they'll have manager-specific packages externalized and they won't be usable in node
+   they won't have types generated for them as they're usually loaded automatically by Storybook
+  */
   if (managerEntries.length) {
     configs.push({
       ...commonConfig,
@@ -44,9 +52,11 @@ export default defineConfig(async (options) => {
     });
   }
 
-  // preview entries are entries meant to be loaded into the preview iframe
-  // they'll have preview-specific packages externalized and they won't be usable in node
-  // they'll have types generated for them so they can be imported when setting up Portable Stories
+  /*
+   preview entries are entries meant to be loaded into the preview iframe
+   they'll have preview-specific packages externalized and they won't be usable in node
+   they'll have types generated for them so they can be imported by users when setting up Portable Stories or using CSF factories
+  */
   if (previewEntries.length) {
     configs.push({
       ...commonConfig,
@@ -57,9 +67,11 @@ export default defineConfig(async (options) => {
     });
   }
 
-  // node entries are entries meant to be used in node-only
-  // this is useful for presets, which are loaded by Storybook when setting up configurations
-  // they won't have types generated for them as they're usually loaded automatically by Storybook
+  /*
+   node entries are entries meant to be used in node-only
+   this is useful for presets, which are loaded by Storybook when setting up configurations
+   they won't have types generated for them as they're usually loaded automatically by Storybook
+  */
   if (nodeEntries.length) {
     configs.push({
       ...commonConfig,
