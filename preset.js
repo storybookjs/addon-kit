44a02a4405b9c5bc6535848147b0aa323d01c2a8
Jeppe Reinhold <jeppe@reinhold.is>
2025-08-21 20:52:29 +0200

    CJS > ESM in root preset.js

--- preset.js
+++ preset.js
@@ -1,4 +1,1 @@
-// this file is slightly misleading. It needs to be CJS, and thus in this "type": "module" package it should be named preset.cjs
-// but Storybook won't pick that filename up so we have to name it preset.js instead
-
-module.exports = require('./dist/preset.cjs');
+export * from "./dist/preset.js";
