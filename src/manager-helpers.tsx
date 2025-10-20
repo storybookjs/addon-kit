5d605afaed8b55319f70651b9ce0c60f9c8fc24d
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-10-20 11:22:51 +0200

    Add manager-helpers example

---
+++ src/manager-helpers.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import type { API_HashEntry } from 'storybook/internal/types'
+import { ADDON_ID } from "./constants";
+
+/**
+ * This opt-in helper can be manually imported by your users who want to
+ * use it, and ignored by those who don't.
+ */
+export function renderLabel(item: API_HashEntry) {
+  if (
+    item.type !== 'story' &&
+    item.type !== 'docs'
+  ) {
+    return
+  }
+
+  if (item.title.startsWith(ADDON_ID)) {
+    return <span>🌟 {item.name}</span>
+  }
+}
