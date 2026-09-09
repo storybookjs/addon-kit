8a7f929193a466f3a071948a3c7cfef057be7381
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2026-09-09 16:19:36 +0200

    Call our own addon's previous annotations in local instance

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,8 +2,11 @@ import { definePreview } from '@storybook/react-vite';
 
 import addonDocs from '@storybook/addon-docs';
 
+// to load the built addon in this test Storybook
+import addonKit from '../dist/index.js';
+
 export default definePreview({
-  addons: [addonDocs()],
+  addons: [addonDocs(), addonKit()],
   initialGlobals: {
     background: { value: 'light' },
   },
