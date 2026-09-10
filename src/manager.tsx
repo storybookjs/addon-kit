c36bd06bcaa5ace50b478665c3f7d5a4a81cc735
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2026-09-10 18:07:24 +0200

    Remove dangling refs to TAB_ID

--- src/manager.tsx
+++ src/manager.tsx
@@ -3,7 +3,7 @@ import { addons, types } from 'storybook/manager-api';
 
 import { Panel } from './components/Panel';
 import { Tool } from './components/Tool';
-import { ADDON_ID, PANEL_ID, TAB_ID, TOOL_ID } from './constants';
+import { ADDON_ID, PANEL_ID, TOOL_ID } from './constants';
 
 /**
  * Note: if you want to use JSX in this file, rename it to `manager.tsx`
@@ -16,7 +16,7 @@ addons.register(ADDON_ID, (api) => {
   addons.add(TOOL_ID, {
     type: types.TOOL,
     title: 'My addon',
-    match: ({ viewMode, tabId }) => !!((viewMode && viewMode.match(/^(story)$/)) || tabId === TAB_ID),
+    match: ({ viewMode }) => !!viewMode?.match(/^(story)$/),
     render: () => <Tool api={api} />,
   });
 
